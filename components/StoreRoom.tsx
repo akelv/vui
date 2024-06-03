c617eeaeadf00ad9443a527a5effc84d9415c788
akelv <kelvin@chotot.vn>
2024-06-03 19:21:21 +0700

    Fix missing ref

--- components/StoreRoom.tsx
+++ components/StoreRoom.tsx
@@ -83,7 +83,7 @@ const StoreRoom: React.FC = () => {
         <div className="absolute bg-gradient-to-t from-black/50 overflow-y-auto to-transparent p-4 flex flex-col items-center justify-center z-10">
             {data && (
               <>
-              <video controls className="mb-4 w-full max-w-md" playsInline> 
+              <video ref={videoRef} controls className="mb-4 w-full max-w-md" playsInline> 
               <source src={data.url} type="video/mp4" /> 
               </video>
                 {data.data.map((product: any, index: number) => (
